b287b4bf3ebec7f006c81b4c2e8de6af0ca2a9b0
The Bright Bot Team <thanintendoking@gmail.com>
2015-09-21 12:05:25 -0400

    Create coreBot.py

---
+++ coreBot.py
@@ -0,0 +1,6 @@
+class TwitterBot(object)://Creating the first bot
+    def __init__(self, authentication_token)
+        print "BrightBot V:0.1"
+        print "Created by Matthew Weidenhamer"
+        print "Last updated 9/21/2015"
+        self.auth_token = authentication_token
