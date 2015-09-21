1641ba536342161a3aef8d59aed265364ea4d3b2
The Bright Bot Team <thanintendoking@gmail.com>
2015-09-21 17:42:25 -0400

    Update coreBot.py

--- coreBot.py
+++ coreBot.py
@@ -1,13 +1,21 @@
 import Tweepy
-class TwitterBot(object)://Creating the first bot
-    
-    def __init__(self, consumer_key  consumer_secret): // You need to supply your own  consumer key and consumer secret key. 
+class TwitterBot(object):
+    def loadQuotes(self, saveFile):
+        self.quotesFile = open(saveFile, "r+")
+        self.quotesNumber = 0;
+        self.quotes = {}
+        while self.quotesFile != "end":
+            self.quotes[self.quotesFile] = self.quotesFile.readline()
+                
+        print("Loaded " + quotesNumber + " quotes!")
+        self.quotesFile.close()
+    def __init__(self, consumer_key  consumer_secret, saveFile):
         print "BrightBot V:0.1"
         print "Created by Matthew Weidenhamer"
         print "Last updated 9/21/2015"
         self.auth = tweepy.0AuthHander(consumer_key, consumer_secret)
         try:
             redirect_url = auth.get_authorization_url
-        except tweety.TweepError:
+        except tweepy.TweepError:
             print "Could not obtain Authorization! This is an error."
-        
+        loadQuotes(saveFile)
